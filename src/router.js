f1559c247274bc22ee357497609814e870da9343
suwan kim <blacksw1102@gmail.com>
2021-03-15 23:19:35 +0900

    Add 라우팅 추가

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "./views/Home"
+import About from "./views/About"
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({  
+   mode: "history",
+   routes: [
+        {path:"/", component: Home},
+        {path:"/about", component: About}
+   ] 
+});
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Welcome to Home}!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "home",
+    data() {
+        return {
+            title: '개발자의 품격',
+        };
+    }
+}
+</script>
