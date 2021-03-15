a1f681990f8cb656df59ed50bdb1ae01d3120d9d
suwan kim <blacksw1102@gmail.com>
2021-03-15 23:26:52 +0900

    Add 데이터 핸들링 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
     <div>
-        <h1>Welcome to Home}!</h1>
+        <h1>Welcome to {{title2}}!</h1>
+        <input type="text" v-model="input1"/>
+        <button type="button" @click="getData()">Get</button>
+        <button type="button" @click="setData()">Set</button>
     </div>
 </template>
-
 <script>
 export default {
     name: "home",
     data() {
         return {
             title: '개발자의 품격',
+            title2: 'Seoul',
+            input1: 'abc',
         };
+    },
+    methods: {
+        getData() {
+            alert(this.input1);
+        },
+        setData() {
+            this.input1 = 12345;
+        }
     }
 }
 </script>
